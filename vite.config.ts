8ac2333a6cabd2d8f8611fabb364c2642e198c55
AnthonyBrito-dev <anthonyrafaelbritobravo10@gmail.com>
2026-04-09 10:11:25 -0500

    Actualizacion de index y resolucion de proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  base:'/project-catapi/',
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        favorites: resolve(__dirname, 'favorites.html'),
+      },
+    },
+  },
 })
